ff92b66d160e496853ed0d82e41d807ce6a0232f
0ankit0 <ankitpaudel.1373@gmail.com>
2026-01-28 20:11:32 +0545

    chore(dependencies): update Pipfile and Pipfile.lock to include setuptools and django-hashid-field

--- ecommerce-backend/config/monitoring.py
+++ ecommerce-backend/config/monitoring.py
@@ -3,7 +3,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.scope import add_global_event_processor
 
-ignore_logger("graphql.execution.utils")
+# ignore_logger("rest.execution.utils")
 
 
 @add_global_event_processor
